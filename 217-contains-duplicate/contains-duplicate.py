db7e0b0999d197fff851b05827d1e77537c466aa
Parth Sharma <parthsharma15116@gmail.com>
2026-08-22 13:37:06 +0530

    Time: 30 ms (42.87%) | Memory: 25.9 MB (70.04%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen=set()
+        for x in nums:
+            if x in seen:
+                return True
+            seen.add(x)
+        return False     
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
