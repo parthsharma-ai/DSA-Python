1236fdcae4b6d3753393a7adc4061895786152b7
Parth Sharma <parthsharma15116@gmail.com>
2026-08-28 23:31:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (72.83%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def runningSum(self, nums):
+        prefix=[0]*len(nums)
+        prefix[0]=nums[0]
+        for i in range(1,len(nums)):
+            prefix[i]=prefix[i-1]+nums[i]
+        return prefix
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
